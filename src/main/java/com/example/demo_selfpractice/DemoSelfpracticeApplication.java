9e36379fc000b9abfafd743ef1b8451a98db9a91
Chetan <chetanbkcbk@gmail.com>
2024-12-05 10:21:39 +0530

    developing on abcbranch

---
+++ src/main/java/com/example/demo_selfpractice/DemoSelfpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_selfpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSelfpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSelfpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_selfpractice/DemoSelfpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_selfpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSelfpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
